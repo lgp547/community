16d33826563ede0ae483c56f7a16d654f02cf080
lgp547 <lgp547279@163.com>
2020-02-06 14:56:03 +0800

    init repo

---
+++ src/main/java/com/example/community/Controller/HelloController.java
@@ -0,0 +1,19 @@
+package com.example.community.Controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@Controller
+public class HelloController {
+    @GetMapping("/hello")
+    public String hell0(@RequestParam(name = "name")String name, Model model){
+        model.addAttribute("name",name);
+        return "hello";
+    }
+    @GetMapping("/test")
+    public String test(){
+        return "hello";
+    }
+}
