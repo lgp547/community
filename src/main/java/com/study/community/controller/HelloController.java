dded461b62c72b1888ef2f607998530eebc590dc
lgp547 <lgp547279@163.com>
2020-03-17 12:26:13 +0800

    0317commit

--- src/main/java/com/study/community/controller/HelloController.java
+++ src/main/java/com/study/community/controller/HelloController.java
@@ -15,17 +15,19 @@ public class HelloController {
     @Autowired(required = false)
     private UserMapper userMapper;
 
-    //响应根目录
+    /**
+     * 响应根目录
+     */
     @GetMapping("/")
-    public String index(HttpServletRequest request){
+    public String index(HttpServletRequest request) {
         //每次进来都进行查询token，和数据库匹配即是登陆
         Cookie[] cookies = request.getCookies();
-        if(cookies != null && cookies.length != 0){
-            for (Cookie cookie : cookies){
-                if(cookie.getName().equals("token")){
+        if (cookies != null && cookies.length != 0) {
+            for (Cookie cookie : cookies) {
+                if (cookie.getName().equals("token")) {
                     User user = userMapper.findByToken(cookie.getValue());
-                    if (user != null){
-                        request.getSession().setAttribute("user",user);
+                    if (user != null) {
+                        request.getSession().setAttribute("user", user);
                     }
                     break;
                 }
@@ -34,4 +36,21 @@ public class HelloController {
 
         return "index";
     }
+
+//    @GetMapping("/hello")
+//    public String hello(@RequestParam(name = "name", required = false, defaultValue = "World") String name, Model model) {
+//        model.addAttribute("name", name);
+//        return "hello";
+//    }
+//    页面响应：
+//    <!DOCTYPE HTML>
+//<html xmlns:th="http://www.thymeleaf.org">
+//<head>
+//    <title>Getting Started: Serving Web Content</title>
+//    <meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
+//</head>
+//<body>
+//<p th:text="'Hello, ' + ${name} + '!'" />
+//</body>
+//</html>
 }
