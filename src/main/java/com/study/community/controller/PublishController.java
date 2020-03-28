26c9479449314bfe38624a9ae4bae3722a2951bd
lgp547 <lgp547279@163.com>
2020-03-28 08:54:49 +0800

    规范化标签

--- src/main/java/com/study/community/controller/PublishController.java
+++ src/main/java/com/study/community/controller/PublishController.java
@@ -1,9 +1,11 @@
 package com.study.community.controller;
 
+import com.study.community.cache.TagCache;
 import com.study.community.dto.QuestionDTO;
 import com.study.community.model.Question;
 import com.study.community.model.User;
 import com.study.community.service.QuestionService;
+import org.apache.commons.lang3.StringUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -28,11 +30,14 @@ public class PublishController {
         model.addAttribute("description", question.getDescription());
         model.addAttribute("tag", question.getTag());
         model.addAttribute("id",question.getId());
+
+        model.addAttribute("tags", TagCache.get());
         return "publish";
     }
 
     @GetMapping("/publish")
-    public String publish(){
+    public String publish(Model model){
+        model.addAttribute("tags", TagCache.get());
         return "publish";
     }
 
@@ -47,6 +52,7 @@ public class PublishController {
         model.addAttribute("title",title);
         model.addAttribute("description",description);
         model.addAttribute("tag",tag);
+        model.addAttribute("tags", TagCache.get());
 
         if (title == null || title == ""){
             model.addAttribute("error","标题不能为空");
@@ -56,10 +62,20 @@ public class PublishController {
             model.addAttribute("error","问题补充不能为空");
             return "publish";
         }
+
+        //规范标签的输入
+        String invalid = TagCache.filterInvalid(tag);
+        if (StringUtils.isNotBlank(invalid)) {
+            model.addAttribute("error", "输入非法标签:" + invalid);
+            return "publish";
+        }
+
         if (tag == null || tag == ""){
             model.addAttribute("error","标签不能为空");
             return "publish";
         }
+
+
 
         User user = (User) request.getSession().getAttribute("user");
 
